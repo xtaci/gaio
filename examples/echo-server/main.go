54c8b08bea1c2d7b5cfc6fa2aa9a08901b280be7
xtaci <daniel820313@gmail.com>
2019-12-23 14:22:30 +0800

    update example

--- examples/echo-server/main.go
+++ examples/echo-server/main.go
@@ -21,16 +21,12 @@ func main() {
 	}
 
 	chRx := make(chan gaio.OpResult)
-	go func() {
-		chTx := make(chan gaio.OpResult)
-		// since read event happens in sequence, so we only
-		// need to make ONE read buffer for a connection.
-		readBuffers := make(map[int][]byte)
+	chTx := make(chan gaio.OpResult)
 
+	go func() {
 		for {
 			select {
 			case res := <-chRx:
-				readBuffers[res.Fd] = res.Buffer
 				// handle unexpected read error
 				if res.Err != nil {
 					log.Println("read error")
@@ -45,13 +41,8 @@ func main() {
 					continue
 				}
 
-				// write events in a watcher also happens in sequence,
-				// make a write buffer for this connection and echo,
-				// only one txBuf exists at one time for a connection
-				txBuf := make([]byte, res.Size)
-				copy(txBuf, res.Buffer)
 				// write the data, we won't start to read again until write completes.
-				w.Write(res.Fd, txBuf, chTx)
+				w.Write(res.Fd, res.Buffer[:res.Size:cap(res.Buffer)], chTx)
 			case res := <-chTx:
 				// handle unexpected write error
 				if res.Err != nil {
@@ -60,7 +51,7 @@ func main() {
 					continue
 				}
 				// write complete, start read again
-				w.Read(res.Fd, readBuffers[res.Fd], chRx)
+				w.Read(res.Fd, res.Buffer[:cap(res.Buffer)], chRx)
 			}
 		}
 	}()

2fc7e46e12e59cfb06f4d2a48e56b3ae4968aa94
xtaci <daniel820313@gmail.com>
2019-12-23 15:48:57 +0800

    add watcher.go

--- aio_linux.go
+++ aio_linux.go
@@ -3,208 +3,41 @@
 package gaio
 
 import (
-	"errors"
-	"log"
-	"net"
-	"sync"
 	"syscall"
 )
 
-var (
-	errRawConn = errors.New("net.Conn does implement net.RawConn")
-)
+type poller int
 
-// aiocb contains all info for a request
-type aiocb struct {
-	fd     int
-	buffer []byte
-	size   int
-	done   chan OpResult
+func createpoll() (poller, error) {
+	fd, err := syscall.EpollCreate1(0)
+	return poller(fd), err
 }
 
-// OpResult of operation
-type OpResult struct {
-	Fd     int
-	Buffer []byte // the original committed buffer
-	Size   int
-	Err    error
+func closepoll(p poller) error {
+	return syscall.Close(int(p))
 }
 
-// Watcher will monitor & process Request(s)
-type Watcher struct {
-	rfd int // epollin & epollout
-	wfd int
-
-	// handlers for a fd(s)
-	readers map[int]aiocb
-	writers map[int]aiocb
-
-	readersLock sync.Mutex
-	writersLock sync.Mutex
-
-	// hold net.Conn to prevent from GC
-	conns     map[int]net.Conn
-	connsLock sync.Mutex
+func poll_in(p poller, fd int) error {
+	return syscall.EpollCtl(int(p), syscall.EPOLL_CTL_ADD, int(fd), &syscall.EpollEvent{Fd: int32(fd), Events: syscall.EPOLLIN})
 }
 
-// CreateWatcher creates a management object for monitoring events of net.Conn
-func CreateWatcher() (*Watcher, error) {
-	w := new(Watcher)
-	rfd, err := syscall.EpollCreate1(0)
-	if err != nil {
-		return nil, err
-	}
-	w.rfd = rfd
-
-	wfd, err := syscall.EpollCreate1(0)
-	if err != nil {
-		return nil, err
-	}
-	w.wfd = wfd
-
-	w.readers = make(map[int]aiocb)
-	w.writers = make(map[int]aiocb)
-	w.conns = make(map[int]net.Conn)
-
-	go w.loopRx()
-	go w.loopTx()
-	return w, nil
+func poll_out(p poller, fd int) error {
+	return syscall.EpollCtl(int(p), syscall.EPOLL_CTL_ADD, int(fd), &syscall.EpollEvent{Fd: int32(fd), Events: syscall.EPOLLOUT})
+}
+func poll_del(p poller, fd int) error {
+	return syscall.EpollCtl(int(p), syscall.EPOLL_CTL_DEL, int(fd), &syscall.EpollEvent{Fd: int32(fd), Events: syscall.EPOLLIN | syscall.EPOLLOUT})
 }
 
-// Close stops monitoring on events for all connections
-func (w *Watcher) Close() error {
-	er := syscall.Close(w.rfd)
-	ew := syscall.Close(w.wfd)
-	if er != nil {
-		return er
-	}
-	if ew != nil {
-		return ew
-	}
-	return nil
-}
-
-// Watch starts watching events on connection `conn`
-func (w *Watcher) Watch(conn net.Conn) (fd int, err error) {
-	c, ok := conn.(interface {
-		SyscallConn() (syscall.RawConn, error)
-	})
-
-	if !ok {
-		return 0, errRawConn
-	}
-
-	rawconn, err := c.SyscallConn()
-	if err != nil {
-		return 0, err
-	}
-
-	var operr error
-	if err := rawconn.Control(func(s uintptr) {
-		fd = int(s)
-	}); err != nil {
-		return 0, err
-	}
-	if operr != nil {
-		return 0, operr
-	}
-
-	w.connsLock.Lock()
-	w.conns[fd] = conn
-	w.connsLock.Unlock()
-	return fd, nil
-}
-
-// StopWatch dereferences net.Conn related to this fd
-func (w *Watcher) StopWatch(fd int) {
-	w.connsLock.Lock()
-	defer w.connsLock.Unlock()
-	delete(w.conns, fd)
-}
-
-// Read submits a read requests to Handle
-func (w *Watcher) Read(fd int, buf []byte, done chan OpResult) error {
-	cb := aiocb{fd: fd, buffer: buf, done: done}
-	w.readersLock.Lock()
-	w.readers[fd] = cb
-	w.readersLock.Unlock()
-	return syscall.EpollCtl(w.rfd, syscall.EPOLL_CTL_ADD, int(fd), &syscall.EpollEvent{Fd: int32(fd), Events: syscall.EPOLLIN})
-}
-
-// Write submits a write requests to Handle
-func (w *Watcher) Write(fd int, buf []byte, done chan OpResult) error {
-	cb := aiocb{fd: fd, buffer: buf, done: done}
-	w.writersLock.Lock()
-	w.writers[fd] = cb
-	w.writersLock.Unlock()
-	return syscall.EpollCtl(w.wfd, syscall.EPOLL_CTL_ADD, int(fd), &syscall.EpollEvent{Fd: int32(fd), Events: syscall.EPOLLOUT})
-}
-
-func (w *Watcher) loopRx() {
+func poll_wait(p poller, callback func(fd int)) error {
 	events := make([]syscall.EpollEvent, 64)
 	for {
-		n, err := syscall.EpollWait(w.rfd, events, -1)
+		n, err := syscall.EpollWait(int(p), events, -1)
 		if err != nil && err != syscall.EINTR {
-			log.Println(err)
-			return
+			return err
 		}
 
 		for i := 0; i < n; i++ {
-			fd := int(events[i].Fd)
-
-			w.readersLock.Lock()
-			cb := w.readers[fd]
-			nr, er := syscall.Read(int(fd), cb.buffer[:])
-			if er == syscall.EAGAIN {
-				w.readersLock.Unlock()
-				continue
-			}
-			result := OpResult{Fd: cb.fd, Buffer: cb.buffer, Size: nr, Err: er}
-			syscall.EpollCtl(w.rfd, syscall.EPOLL_CTL_DEL, int(fd), &syscall.EpollEvent{Fd: int32(fd), Events: syscall.EPOLLIN})
-			w.readersLock.Unlock()
-
-			if cb.done != nil {
-				cb.done <- result
-			}
-		}
-	}
-}
-
-func (w *Watcher) loopTx() {
-	events := make([]syscall.EpollEvent, 64)
-	for {
-		n, err := syscall.EpollWait(w.wfd, events, -1)
-		if err != nil && err != syscall.EINTR {
-			log.Println(err)
-			return
-		}
-
-		for i := 0; i < n; i++ {
-			fd := int(events[i].Fd)
-
-			w.writersLock.Lock()
-			cb := w.writers[fd]
-			nw, ew := syscall.Write(int(fd), cb.buffer)
-			if ew == syscall.EAGAIN {
-				w.writersLock.Unlock()
-				continue
-			}
-
-			if ew == nil {
-				cb.size += nw
-			}
-
-			if len(cb.buffer) == cb.size || ew != nil { // done
-				syscall.EpollCtl(w.wfd, syscall.EPOLL_CTL_DEL, int(fd), &syscall.EpollEvent{Fd: int32(fd), Events: syscall.EPOLLOUT})
-				w.writersLock.Unlock()
-
-				if cb.done != nil {
-					cb.done <- OpResult{Fd: cb.fd, Buffer: cb.buffer, Size: cb.size, Err: ew}
-				}
-			} else {
-				w.writers[fd] = cb
-				w.writersLock.Unlock()
-			}
+			callback(int(events[i].Fd))
 		}
 	}
 }
---
+++ watcher.go
@@ -0,0 +1,189 @@
+package gaio
+
+import (
+	"errors"
+	"net"
+	"sync"
+	"syscall"
+)
+
+var (
+	errRawConn = errors.New("net.Conn does implement net.RawConn")
+)
+
+// aiocb contains all info for a request
+type aiocb struct {
+	fd     int
+	buffer []byte
+	size   int
+	done   chan OpResult
+}
+
+// OpResult of operation
+type OpResult struct {
+	Fd     int
+	Buffer []byte // the original committed buffer
+	Size   int
+	Err    error
+}
+
+// Watcher will monitor & process Request(s)
+type Watcher struct {
+	rfd poller // epollin & epollout
+	wfd poller
+
+	// handlers for a fd(s)
+	readers map[int]aiocb
+	writers map[int]aiocb
+
+	readersLock sync.Mutex
+	writersLock sync.Mutex
+
+	// hold net.Conn to prevent from GC
+	conns     map[int]net.Conn
+	connsLock sync.Mutex
+}
+
+// CreateWatcher creates a management object for monitoring events of net.Conn
+func CreateWatcher() (*Watcher, error) {
+	w := new(Watcher)
+	rfd, err := createpoll()
+	if err != nil {
+		return nil, err
+	}
+	w.rfd = rfd
+
+	wfd, err := createpoll()
+	if err != nil {
+		return nil, err
+	}
+	w.wfd = wfd
+
+	w.readers = make(map[int]aiocb)
+	w.writers = make(map[int]aiocb)
+	w.conns = make(map[int]net.Conn)
+
+	go w.loopRead()
+	go w.loopWrite()
+	return w, nil
+}
+
+// Close stops monitoring on events for all connections
+func (w *Watcher) Close() error {
+	er := closepoll(w.rfd)
+	ew := closepoll(w.wfd)
+	if er != nil {
+		return er
+	}
+	if ew != nil {
+		return ew
+	}
+	return nil
+}
+
+// StopWatch dereferences net.Conn related to this fd
+func (w *Watcher) StopWatch(fd int) {
+	w.connsLock.Lock()
+	defer w.connsLock.Unlock()
+	delete(w.conns, fd)
+}
+
+// Read submits a read requests to Handle
+func (w *Watcher) Read(fd int, buf []byte, done chan OpResult) error {
+	cb := aiocb{fd: fd, buffer: buf, done: done}
+	w.readersLock.Lock()
+	w.readers[fd] = cb
+	w.readersLock.Unlock()
+	return poll_in(w.rfd, fd)
+}
+
+// Write submits a write requests to Handle
+func (w *Watcher) Write(fd int, buf []byte, done chan OpResult) error {
+	cb := aiocb{fd: fd, buffer: buf, done: done}
+	w.writersLock.Lock()
+	w.writers[fd] = cb
+	w.writersLock.Unlock()
+	return poll_out(w.wfd, fd)
+}
+
+// Watch starts watching events on connection `conn`
+func (w *Watcher) Watch(conn net.Conn) (fd int, err error) {
+	c, ok := conn.(interface {
+		SyscallConn() (syscall.RawConn, error)
+	})
+
+	if !ok {
+		return 0, errRawConn
+	}
+
+	rawconn, err := c.SyscallConn()
+	if err != nil {
+		return 0, err
+	}
+
+	var operr error
+	if err := rawconn.Control(func(s uintptr) {
+		fd = int(s)
+	}); err != nil {
+		return 0, err
+	}
+	if operr != nil {
+		return 0, operr
+	}
+
+	w.connsLock.Lock()
+	w.conns[fd] = conn
+	w.connsLock.Unlock()
+	return fd, nil
+}
+
+func (w *Watcher) loopRead() {
+	callback := func(fd int) {
+		w.readersLock.Lock()
+		cb := w.readers[fd]
+		nr, er := syscall.Read(int(fd), cb.buffer[:])
+		if er == syscall.EAGAIN {
+			w.readersLock.Unlock()
+			return
+		}
+		result := OpResult{Fd: cb.fd, Buffer: cb.buffer, Size: nr, Err: er}
+		poll_del(w.rfd, fd)
+		w.readersLock.Unlock()
+
+		if cb.done != nil {
+			cb.done <- result
+		}
+	}
+
+	poll_wait(w.rfd, callback)
+}
+
+func (w *Watcher) loopWrite() {
+	callback := func(fd int) {
+		w.writersLock.Lock()
+		cb := w.writers[fd]
+		nw, ew := syscall.Write(fd, cb.buffer)
+		if ew == syscall.EAGAIN {
+			w.writersLock.Unlock()
+			return
+		}
+
+		if ew == nil {
+			cb.size += nw
+		}
+
+		if len(cb.buffer) == cb.size || ew != nil { // done
+			poll_del(w.wfd, fd)
+			w.writersLock.Unlock()
+
+			if cb.done != nil {
+				cb.done <- OpResult{Fd: cb.fd, Buffer: cb.buffer, Size: cb.size, Err: ew}
+			}
+		} else {
+			w.writers[fd] = cb
+			w.writersLock.Unlock()
+		}
+	}
+
+	poll_wait(w.wfd, callback)
+}

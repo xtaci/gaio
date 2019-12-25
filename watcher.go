10a79ec366a7663d8517a42811beae1e0630437b
xtaci <daniel820313@gmail.com>
2019-12-25 16:20:00 +0800

    update

--- watcher.go
+++ watcher.go
@@ -1,6 +1,6 @@
-// Package gaio is an Async-IO library for golang for Golang.
+// Package gaio is an Async-IO library for Golang.
 //
-// gaio acts in proactor mode, https://en.wikipedia.org/wiki/Proactor_pattern .
+// gaio acts in proactor mode, https://en.wikipedia.org/wiki/Proactor_pattern.
 // User submit async IO operations and waits for IO-completion signal.
 package gaio
 

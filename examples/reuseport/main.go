d9a18944ff23f61b8323ad6e8154a3a2d6843c6f
xtaci <daniel820313@gmail.com>
2020-01-07 11:49:37 +0800

    add example for reuseport

---
+++ examples/reuseport/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/libp2p/go-reuseport"
+	"github.com/xtaci/gaio"
+)
+
+func main() {
+	// simply replace net.Listen with reuseport.Listen
+	ln, err := reuseport.Listen("tcp", "localhost:0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("pushing server listening on", ln.Addr())
+
+	w, err := gaio.CreateWatcher(4096)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// chanel
+	ticker := time.NewTicker(time.Second)
+	chFd := make(chan int)
+	chIO := make(chan gaio.OpResult)
+
+	go func() {
+		for {
+			res, err := w.WaitIO()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			chIO <- res
+		}
+	}()
+
+	go func() {
+		fds := make(map[int]bool)
+		for {
+			select {
+			case res := <-chIO:
+				if res.Err != nil {
+					delete(fds, res.Fd)
+				}
+			case t := <-ticker.C:
+				push := []byte(fmt.Sprintf("%s\n", t))
+				for fd := range fds {
+					w.Write(nil, fd, push)
+				}
+			case fd := <-chFd:
+				fds[fd] = true
+			}
+		}
+	}()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		fd, err := w.NewConn(conn)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println("new client", conn.RemoteAddr())
+		chFd <- fd
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/xtaci/gaio
 
 go 1.13
+
+require github.com/libp2p/go-reuseport v0.0.1

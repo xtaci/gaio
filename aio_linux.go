890a3e7d07cc2e104e3bc8d331cb0eee5b2a22d3
xtaci <daniel820313@gmail.com>
2021-04-16 12:01:04 +0800

    upd

--- aio_linux.go
+++ aio_linux.go
@@ -117,6 +117,7 @@ func (p *poller) wakeup() error {
 }
 
 func (p *poller) Wait(chEventNotify chan pollerEvents) {
+	setAffinity()
 	p.initCache(cap(chEventNotify) + 2)
 	events := make([]syscall.EpollEvent, maxEvents)
 	// close poller fd & eventfd in defer

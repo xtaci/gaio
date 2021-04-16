aca5539986c279f91cf4f4a1992f8c9a9be65e38
xtaci <daniel820313@gmail.com>
2021-04-16 23:00:58 +0800

    test same bind

--- affinity_linux.go
+++ affinity_linux.go
@@ -8,25 +8,22 @@ package gaio
 #include <pthread.h>
 
 void lock_thread(int cpuid) {
-        pthread_t tid;
-        cpu_set_t cpuset;
+	pthread_t tid;
+	cpu_set_t cpuset;
 
-        tid = pthread_self();
-        CPU_ZERO(&cpuset);
-        CPU_SET(cpuid, &cpuset);
-    pthread_setaffinity_np(tid, sizeof(cpu_set_t), &cpuset);
+	tid = pthread_self();
+	CPU_ZERO(&cpuset);
+	CPU_SET(cpuid, &cpuset);
+	pthread_setaffinity_np(tid, sizeof(cpu_set_t), &cpuset);
 }
 */
 import "C"
 import (
-	"crypto/rand"
 	"runtime"
 )
 
 // bind thread & goroutine to a specific CPU
 func setAffinity() {
-	b := make([]byte, 1)
-	rand.Read(b)
 	runtime.LockOSThread()
-	C.lock_thread(C.int(b[0]))
+	C.lock_thread(C.int(1))
 }

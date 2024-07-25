678e9f354fefd7648b3ae12d0e1b30860cd99287
xtaci <daniel820313@gmail.com>
2024-07-25 21:49:52 +0800

    add license to source codes

---
+++ pkg.go
@@ -0,0 +1,5 @@
+// Package gaio is an Async-IO library for Golang.
+//
+// gaio acts in proactor mode, https://en.wikipedia.org/wiki/Proactor_pattern.
+// User submit async IO operations and waits for IO-completion signal.
+package gaio
--- watcher.go
+++ watcher.go
@@ -1,10 +1,25 @@
-//go:build linux || darwin || netbsd || freebsd || openbsd || dragonfly
-// +build linux darwin netbsd freebsd openbsd dragonfly
-
-// Package gaio is an Async-IO library for Golang.
+// The MIT License (MIT)
 //
-// gaio acts in proactor mode, https://en.wikipedia.org/wiki/Proactor_pattern.
-// User submit async IO operations and waits for IO-completion signal.
+// Copyright (c) 2019 xtaci
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package gaio
 
 import (

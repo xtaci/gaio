8ca11793689a8b41ba5c841647d534f3a6482d83
xtaci <imap@live.com>
2024-12-20 15:47:58 +0800

    use sys/unix instead of CGO

--- affinity_linux.go
+++ affinity_linux.go
@@ -24,33 +24,18 @@
 
 package gaio
 
-/*
-#define _GNU_SOURCE
-#include <sched.h>
-#include <pthread.h>
-
-// lock_thread sets the CPU affinity for the calling thread to the specified CPU.
-// This ensures that the thread runs only on the designated CPU, which can help
-// with performance optimization and consistent execution timing on multi-core systems.
-void lock_thread(int cpuid) {
-	pthread_t tid;
-	cpu_set_t cpuset;
-
-	tid = pthread_self();               // Get the ID of the calling thread
-	CPU_ZERO(&cpuset);                  // Initialize the CPU set to be empty
-	CPU_SET(cpuid, &cpuset);            // Add the specified CPU to the set
-	pthread_setaffinity_np(tid, sizeof(cpu_set_t), &cpuset); // Set the thread's CPU affinity
-}
-*/
-import "C"
 import (
 	"runtime"
+
+	"golang.org/x/sys/unix"
 )
 
 // setAffinity binds the current goroutine and its underlying thread to a specific CPU.
 // This function locks the goroutine to the current thread, then sets the thread's CPU affinity
 // to the specified CPU, which can improve performance by reducing context switching.
 func setAffinity(cpuId int32) {
-	runtime.LockOSThread()      // Lock the current goroutine to its current thread
-	C.lock_thread(C.int(cpuId)) // Set the thread's CPU affinity to the specified CPU
+	var newMask unix.CPUSet
+	newMask.Set(int(cpuId))
+	runtime.LockOSThread() // Lock the current goroutine to its current thread
+	_ = unix.SchedSetaffinity(0, &newMask)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/xtaci/gaio
 
 go 1.13
+
+require golang.org/x/sys v0.28.0 // indirect
